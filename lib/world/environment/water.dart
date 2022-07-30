9ec0d38af457626eb9fa71815bbacf99f29d4e61
Alexey Volkov <asgalex@gmail.com>
2022-07-30 04:59:13 +0300

    more lightweight water

--- lib/world/environment/water.dart
+++ lib/world/environment/water.dart
@@ -3,8 +3,7 @@ import 'package:flame/components.dart';
 import 'package:tank_game/packages/tiled_utils/lib/tiled_utils.dart';
 import 'package:tank_game/world/world.dart';
 
-class WaterCollide extends SpriteComponent
-    with CollisionCallbacks, DestroyableComponent {
+class WaterCollide extends PositionComponent with CollisionCallbacks {
   WaterCollide(this.tileProcessor, {super.position, super.size})
       : super(priority: RenderPriority.water.priority);
 
@@ -12,7 +11,6 @@ class WaterCollide extends SpriteComponent
 
   @override
   Future<void> onLoad() async {
-    sprite = await tileProcessor.getSprite();
     final collision = tileProcessor.getCollisionRect();
     if (collision != null) {
       collision.collisionType = CollisionType.passive;
