0c01b8a4986e41be077965e8d9335905e192c7cb
Alexey Volkov <asgalex@gmail.com>
2023-09-04 15:36:23 +0300

    lost files

---
+++ assets/tiles/radar_head.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="radar_head" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="../images/redar_head.png" width="64" height="16"/>
+ <tile id="0" type="radar_head">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="3000"/>
+  </animation>
+ </tile>
+ <tile id="1" type="radar_head_disabled"/>
+ <tile id="2" type="radar_ground"/>
+ <tile id="3" type="radar_head_wreck"/>
+</tileset>
